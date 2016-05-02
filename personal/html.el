48431a2e3e65eb084fbfc241307c376d0fad6507
Oskar Wickström <oskar.wickstrom@gmail.com>
2016-05-02 09:55:15 +0200

    Clean up

--- personal/html.el
+++ personal/html.el
@@ -3,3 +3,7 @@
             ;; Default indentation to 2, but let SGML mode guess, too.
             (set (make-local-variable 'sgml-basic-offset) 2)
             (sgml-guess-indent)))
+
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'html-mode-hook 'emmet-mode)
+(add-hook 'web-mode-hook 'emmet-mode)
--- personal/ido.el
+++ personal/ido.el
@@ -1,3 +1,7 @@
+;; Fix "Pinging *.id (Indonesia) problem.
+;; https://github.com/technomancy/emacs-starter-kit/issues/39
+(setq ido-use-filename-at-point nil)
+
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
