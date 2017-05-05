c3749889e5f188141467c502931ae73c5cfd781f
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-05-05 19:44:18 +0200

    Use tomorrow-night and evil-mode

--- personal/preload/ui.el
+++ personal/preload/ui.el
@@ -4,7 +4,7 @@
 
 (when window-system
   ;; GUI Customizations
-  (setq-default line-spacing 0)
+  (setq-default line-spacing 2)
   (set-face-attribute 'default nil :font "Fira Code-14")
   (tool-bar-mode -1)
   (scroll-bar-mode -1))
