56e02da4ad6fc2d21882f442bfb4686eb0276f2a
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-06-14 07:14:38 +0200

    Add custom settings for Windows

--- personal/preload/ui.el
+++ personal/preload/ui.el
@@ -1,10 +1,19 @@
 (setq prelude-theme nil)
 
-(menu-bar-mode -1)
+(menu-bar-mode 1)
 
-(when window-system
-  ;; GUI Customizations
-  (setq-default line-spacing 0)
-  (set-frame-font "Fira Code 13")
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1))
+;; GUI Customizations
+(pcase window-system
+  ((guard (not 'window-system))
+   (message "No window system, skipping GUI customization."))
+  (w32
+   (setq-default line-spacing 0)
+   (set-frame-font "Consolas 12")
+   (tool-bar-mode -1)
+   (scroll-bar-mode -1))
+  ('other
+   (setq-default line-spacing 0)
+   (set-frame-font "Fira Code 13")
+   (tool-bar-mode -1)
+   (scroll-bar-mode -1))
+  )
