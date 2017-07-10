c8665c21546235fab0e6441b74e686693eec94e7
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-07-10 09:53:17 +0200

    Use Hlint together with intero

--- personal/haskell.el
+++ personal/haskell.el
@@ -1,6 +1,7 @@
 (prelude-require-packages '(haskell-mode
                             hindent
-                            intero))
+                            intero
+                            flycheck))
 
 (setenv "PATH" (concat (getenv "PATH") ":~/.local/bin"))
 (setq exec-path (append exec-path '("~/.local/bin")))
@@ -11,3 +12,6 @@
 (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
 (add-hook 'haskell-mode-hook #'hindent-mode)
 (add-hook 'haskell-mode-hook 'intero-mode)
+
+(with-eval-after-load 'intero
+  (flycheck-add-next-checker 'intero '(warning . haskell-hlint)))
