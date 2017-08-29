1554c2786c8112566c06f7080de6b2a1ec703cf3
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-08-29 12:23:21 +0200

    Intero and haskell fixes

--- personal/haskell.el
+++ personal/haskell.el
@@ -15,3 +15,13 @@
 
 (with-eval-after-load 'intero
   (flycheck-add-next-checker 'intero '(warning . haskell-hlint)))
+
+(defun intero-change-directory ()
+  (interactive)
+  (intero-with-repl-buffer nil
+    (comint-simple-send
+     (get-buffer-process (current-buffer))
+     (concat ":cd " (read-directory-name "Directory to use in Intero REPL:")))))
+
+(add-hook 'intero-mode-hook (lambda ()
+                              (global-set-key (kbd "C-<f5>") #'intero-devel-reload)))
--- personal/purescript.el
+++ personal/purescript.el
@@ -1,11 +1,13 @@
 (prelude-require-packages '(purescript-mode
                             psc-ide
-                            psci))
+                            psci
+                            flycheck))
 
 (add-hook 'purescript-mode-hook
           (lambda ()
             (psc-ide-mode)
             (company-mode)
+            (flycheck-mode)
             (turn-on-purescript-indentation)
             ;;(turn-on-purescript-unicode-input-method)
             ))
