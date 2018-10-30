2ba443645d23ab3c2458492da91185d03eedaa39
Oskar Wickström <oskar.wickstrom@gmail.com>
2018-10-30 10:02:33 +0100

    MBP larger font

--- personal/preload/ui.el
+++ personal/preload/ui.el
@@ -1,11 +1,11 @@
 (setq prelude-guru nil)
 (setq prelude-theme nil)
 
-(add-to-list 'default-frame-alist '(font . "Iosevka-12"))
+(add-to-list 'default-frame-alist '(font . "Iosevka-15"))
 
 (when window-system
   (setq-default line-spacing 0)
-  (set-frame-font "Iosevka 12")
+  (set-frame-font "Iosevka 15")
   (menu-bar-mode -1)
   (tool-bar-mode -1)
   (scroll-bar-mode -1))
